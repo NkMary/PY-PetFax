a7e3887efbedc6ab49918e2b60be5810be301943
Nkechinyere Urum-Eke <nk@NkechinyeresMBP.lan>
2023-10-28 18:53:28 -0400

    updated new html page

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint, render_template, request, redirect)
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/new')
 def form():
-    return render_template('pet_form.html')
+    return render_template('facts/new.html')
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(f"request.form {request.form}")
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+    results=models.Fact.query.all()
+  
+    return render_template('facts/index.html', facts=results)
